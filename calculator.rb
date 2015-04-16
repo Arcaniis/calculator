be80651c21c123b035e6f3a2a617007c1dcce3a3
Arcaniis <mullinsdavid88@gmail.com>
2015-04-15 23:14:35 -0400

    recommended changes

--- calculator.rb
+++ calculator.rb
@@ -5,30 +5,34 @@ puts "This program will ask you for a first number and a second number.
 After that, please choose the operator to use with the two numbers."
 
 # Just a method to add artifical load time to allow time to read.
-def loading
+def show_loading_dots
   3.times do 
     sleep(0.5) 
     print "."
   end
 end
 
-loading
+show_loading_dots
 
 puts "\nPlease enter your first number:"
 num1 = gets.to_f
 
-loading
+show_loading_dots
 
 puts "\nPlease enter your second number:"
 num2 = gets.to_f
 
-loading
+show_loading_dots
 
 operator = 0
 
 until operator == 1 || operator == 2 || operator == 3 || operator == 4
-puts "\nPlease choose your operation:\n1) +\n2) -\n3) x\n4) /"
-operator = gets.chomp.to_i
+  puts "Please choose your operation:"
+  puts "1) +"
+  puts "2) -"
+  puts "3) x"
+  puts "4) /"
+  operator = gets.chomp.to_i
 end
 
 case operator
